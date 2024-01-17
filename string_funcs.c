33913626faa1c227464f3a865b6028502290e2aa
IlyassBacime <ibacime@gmail.com>
2024-01-16 18:58:08 -0800

    string file done

---
+++ string_funcs.c
@@ -0,0 +1,88 @@
+#include "monty.h"
+
+/**
+ * _strcmp - Compares both strings
+ * @s1: var
+ * @s2: var
+ * Return: 0 if s1 and s2 are equals
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] == s2[i] && s1[i]; i++)
+		;
+	if (s1[i] > s2[i])
+		return (1);
+	if (s1[i] < s2[i])
+		return (-1);
+	return (0);
+}
+
+/**
+ * _sch - Looks for a char
+ * @s: str
+ * @c: var
+ * Return: 1 if success 0 if failure
+ */
+
+int _sch(char *s, char c)
+{
+	int cont = 0;
+
+	while (s[cont] != '\0')
+	{
+		if (s[cont] == c)
+		{
+			break;
+		}
+		cont++;
+	}
+	if (s[cont] == c)
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * _strtoky - Cuts 2 strings
+ * @s: var
+ * @d: var
+ * Return: 1st part
+ */
+
+char *_strtoky(char *s, char *d)
+{
+	static char *ultimo;
+	int i = 0, j = 0;
+
+	if (!s)
+		s = ultimo;
+	while (s[i] != '\0')
+	{
+		if (_sch(d, s[i]) == 0 && s[i + 1] == '\0')
+		{
+			ultimo = s + i + 1;
+			*ultimo = '\0';
+			s = s + j;
+			return (s);
+		}
+		else if (_sch(d, s[i]) == 0 && _sch(d, s[i + 1]) == 0)
+			i++;
+		else if (_sch(d, s[i]) == 0 && _sch(d, s[i + 1]) == 1)
+		{
+			ultimo = s + i + 1;
+			*ultimo = '\0';
+			ultimo++;
+			s = s + j;
+			return (s);
+		}
+		else if (_sch(d, s[i]) == 1)
+		{
+			j++;
+			i++;
+		}
+	}
+	return (NULL);
+}
